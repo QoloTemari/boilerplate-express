0df4a92b9189c19614b85a531ee44e8aff72334c
qolotemari@outlook.com <MyGithyb1stPW!>
2021-06-17 22:40:03 -0400

    Lesson 6: Use the .env File

--- myApp.js
+++ myApp.js
@@ -1,16 +1,20 @@
 var express = require('express');
 var app = express();
-//console.log("Hello World");
+
 app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
   });
 
-  app.get("/json", function(req, res) {
-    res.send({"message": "Hello json"});
+ app.get("/json", function(req, res) {
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+      res.send({"message": "Hello json".toUpperCase()})
+    } else {
+      res.send({"message": "Hello json"})
+    }
+
   });
-
-
 
 
 
