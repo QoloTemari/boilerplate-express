ca10066efda93753c2ffa480bd4f3a74a4c4ae15
qolotemari@outlook.com <MyGithyb1stPW!>
2021-06-18 02:25:39 -0400

    Lesson 7: Implement a Root-Level Request Logger Middleware

--- myApp.js
+++ myApp.js
@@ -3,6 +3,11 @@ var app = express();
 
 app.use("/public", express.static(__dirname + "/public"));
 
+app.use(function(req, res, next){
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+  })
+  
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
   });
@@ -16,6 +21,7 @@ app.get("/", function(req, res) {
 
   });
 
+ 
 
 
 
