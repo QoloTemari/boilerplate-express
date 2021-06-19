cb70f52e8f006a6cca2fddf1d8e2f2b46dd55427
qolotemari@outlook.com <MyGithyb1stPW!>
2021-06-18 23:36:03 -0400

    Lesson 8: Chain Middleware to Create a Time Server

--- myApp.js
+++ myApp.js
@@ -7,7 +7,15 @@ app.use(function(req, res, next){
     console.log(req.method + " " + req.path + " - " + req.ip);
     next();
   })
-  
+
+  app.get("/now", function(req, res, next){
+    req.time = new Date().toString();
+    next();
+  },
+    function(req, res){
+      res.send({"time": req.time});
+  });
+
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
   });
